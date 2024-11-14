04db7f50615a2d8791b8873893e8d7ca988662ed
V <arunvel.v@capgemini.com>
2024-11-14 16:53:27 +0530

    first commit

---
+++ src/activity/Application.java
@@ -0,0 +1,18 @@
+package activity;
+ 
+import java.util.Arrays;
+ 
+public class Application {
+	public double simpleInterest(double p, double n, double r) {
+		return (p*n*r)/100;
+	}
+	public double discount(double price, double percent) {
+		return (price*percent)/100;
+	}
+	public int minValue(int[] arr) {
+		Arrays.sort(arr);
+		return arr[0];
+	}
+}
+ 
+ 
